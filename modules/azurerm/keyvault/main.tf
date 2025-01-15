775b9d795780945665a1cf422ececcf1db04f51d
Mark Railton <markjasonrailton@gmail.com>
2025-01-15 13:41:18 +0000

    updates: data fetch tenant id, disable soft delete

--- modules/azurerm/keyvault/main.tf
+++ modules/azurerm/keyvault/main.tf
@@ -1,19 +1,41 @@
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
+
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "keyvault" {
   name                        = var.name
   location                    = var.location
   resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = var.sku_name
-
-  soft_delete_enabled         = true
+  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
-  tags                        = var.tags
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
+
 
 output "key_vault_id" {
   value = azurerm_key_vault.keyvault.id
