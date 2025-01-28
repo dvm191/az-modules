a24d2e1e4315987724eb58bec9cf63b7edd60bac
Dayne Segal <dayne.segal@lexisnexis.co.uk>
2025-01-28 15:10:17 +0000

    vpc peering module

---
+++ dayne-modules/azurerm/modules/vnet-peering/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network_peering" "source_to_destination" {
+  name                      = var.peering_name
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = var.source_vnet_id
+  remote_virtual_network_id = var.destination_vnet_id
+
+  allow_forwarded_traffic = true
+  allow_gateway_transit   = false
+  use_remote_gateways     = false
+}
+
+resource "azurerm_virtual_network_peering" "destination_to_source" {
+  name                      = "${var.peering_name}-reverse"
+  resource_group_name       = var.resource_group_name
+  virtual_network_name      = var.destination_vnet_id
+  remote_virtual_network_id = var.source_vnet_id
+
+  allow_forwarded_traffic = true
+  allow_gateway_transit   = false
+  use_remote_gateways     = false
+}
---
+++ dayne-modules/azurerm/modules/vnet-peering/outputs.tf
@@ -0,0 +1,7 @@
+output "source_to_destination_peering_id" {
+  value = azurerm_virtual_network_peering.source_to_destination.id
+}
+
+output "destination_to_source_peering_id" {
+  value = azurerm_virtual_network_peering.destination_to_source.id
+}
