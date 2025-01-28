b4a61de8a29e493a5e99d01c681b1b422629b6b6
Dayne Segal <dayne.segal@lexisnexis.co.uk>
2025-01-28 14:50:31 +0000

    correcting variable names

--- dayne-modules/azurerm/modules/bastion/main.tf
+++ dayne-modules/azurerm/modules/bastion/main.tf
@@ -1,30 +1,24 @@
 data "azurerm_subscription" "current" {}
 
-locals {
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  virtual_network_name = var.vnet_name
-}
-
 resource "azurerm_subnet" "BastionSubnet" {
   name                 = "AzureBastionSubnet"
-  resource_group_name  = local.resource_group_name
-  virtual_network_name = local.virtual_network_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
   address_prefixes     = var.subnet_address_prefixes
 }
 
 resource "azurerm_public_ip" "address" {
   name                = var.public_ip_name
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "build" {
   name                = var.bastion_name
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                 = "configuration"
