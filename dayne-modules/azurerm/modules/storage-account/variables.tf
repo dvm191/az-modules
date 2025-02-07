d45be9ed7b7fe4e08068d5733a32dbe87e969c3b
Mark Railton <markjasonrailton@gmail.com>
2025-02-07 18:11:25 +0000

    updates

--- dayne-modules/azurerm/modules/storage-account/variables.tf
+++ dayne-modules/azurerm/modules/storage-account/variables.tf
@@ -19,10 +19,40 @@ variable "account_tier" {
   default     = "Standard"
 }
 
-variable "account_replication_type" {
+variable "accountReplicationType" {
   description = "The replication type of the storage account."
   type        = string
   default     = "LRS"
+}
+
+variable "accountTier" {
+  type = string
+  default = "Standard"
+}
+
+variable "accessTier" {
+  type = string
+  default = "HOT"
+}
+
+variable "httpsTrafficOnlyEnabled" {
+  type = string
+}
+
+variable "minTlsVersion" {
+  type = string
+}
+
+variable "allowNestedItemsToBePublic" {
+  type = bool
+}
+
+variable "publicNetworkAccessEnabled" {
+  type = bool
+}
+
+variable "infrastructureEncryptionEnabled" {
+  type = bool
 }
 
 variable "tags" {
